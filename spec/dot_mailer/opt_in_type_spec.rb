02fb258c0006dadd3e7c2136889b29e6a96690d3
Warren Sangster <warren@moneyspyder.co.uk>
2017-11-17 15:36:30 +0000

    SR-892 Rspec update

--- spec/dot_mailer/opt_in_type_spec.rb
+++ spec/dot_mailer/opt_in_type_spec.rb
@@ -8,13 +8,13 @@ describe DotMailer::OptInType do
       context 'when the opt in type exists' do
         let(:opt_in_type) { DotMailer::OptInType::SINGLE }
 
-        specify { subject.exists?(opt_in_type).should be_true }
+        specify { subject.exists?(opt_in_type).should be true }
       end
 
       context 'when the opt in type doesnt exist' do
         let(:opt_in_type) { 'Something Random' }
 
-        specify { subject.exists?(opt_in_type).should be_false }
+        specify { subject.exists?(opt_in_type).should be false }
       end
     end
   end
